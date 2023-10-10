4533d92323def08814e0284dbcbd8595bff8e4b3
GalehPamungkas12 <galehpamungkas145@gmail.com>
2023-10-10 08:45:58 +0700

    Pertemuan 2

---
+++ application/controllers/Contoh.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo "nama saya galeh pamungkas 
+        saya tinggal di citayem hobi saya bermain game ";
+    }
+}
